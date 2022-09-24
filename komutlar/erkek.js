4d355a284c242733efda6f76af08e4c4a2673563
Holms <79160693+Shnex1@users.noreply.github.com>
2022-09-24 19:05:40 +0300

    erkek kayıt

--- komutlar/erkek.js
+++ komutlar/erkek.js
@@ -2,14 +2,14 @@ module.exports = {
     name:"e",
     aliased:["man", "erkek", "boy"],
     code:`
-    $addCmdReactions[<:limon:1016758552867635200>]
-    $giveRole[786692350255693824;$mentioned[1];1020058834443714640]
-    $takeRole[786692350255693824;$mentioned[1];1020058831247646760]
+    $addCmdReactions[emoji ismi]
+    $giveRole[sunucu id;$mentioned[1];erkekrolid]
+    $takeRole[sunucu id;$mentioned[1];kayıtsızrolid]
     
     $changeNickname[$mentioned[1]; $noMentionMessage[1] | $noMentionMessage[2]]
    
-    $channelSendMessage[1023204761257910323;<@$mentioned[1]> Aramıza Katıldı Ona Hoş Geldin Diyelim <#1019690772565610567> Kanalını Okumuş Sayılacaksın. İyi Eğlenceler.]
-    $channelSendMessage[1023204853004107838;
+    $channelSendMessage[chatid;<@$mentioned[1]> Aramıza Katıldı Ona Hoş Geldin Diyelim <#kurallarkanalıid> Kanalını Okumuş Sayılacaksın. İyi Eğlenceler.]
+    $channelSendMessage[kadınkayıtlog;
    
     **Kayıt Edilen Kullanıcı:** \`$userTag[$mentioned[1]]\` \`($mentioned[1])\`
     
@@ -18,7 +18,7 @@ module.exports = {
     **Kullanıcının Yeni İsmi:** \` $noMentionMessage[1] | $noMentionMessage[2]\`
     
     **Kayıt Türü:** \`Erkek ♂️\`
-    **Verilen Rol:** \`$roleName[1020058834443714640]\`
+    **Verilen Rol:** \`$roleName[erkekrolid]\`
     
        
     $onlyIf[$noMentionMessage[2]!=;
@@ -30,7 +30,7 @@ module.exports = {
     $onlyIf[$mentioned[1]!=;
     **Bir Kişi Etiketleyin!**{delete:7s}]
 
-    $onlyForRoles[1020058833281876059;Bu komuda **ERİŞİM** yektin yok.]
-    $onlyForServers[786692350255693824;]
+    $onlyForRoles[kayıtyetkilisiid;Bu komuda **ERİŞİM** yektin yok.]
+    $onlyForServers[sunucuid;]
     `
     }
